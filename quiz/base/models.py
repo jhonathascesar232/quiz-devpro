a25845e177a2104b2c736ec39009e8b1384a5319
Jhonathas César <jhonathascesar232@gmail.com>
2021-03-26 20:44:33 -0300

    Adicionado templates

--- quiz/base/models.py
+++ quiz/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pergunta(models.Model):
+	ALTERNATIVAS = [
+		(0, 'A'),
+		(1, 'B'),
+		(2, 'C'),
+		(3, 'D'),
+	]
+	enunciado = models.TextField()
+	disponivel = models.BooleanField(default = False)
+	alternativas = models.JSONField()
+	alternativa_correta = models.IntegerField(ALTERNATIVAS)
--- quiz/base/views.py
+++ quiz/base/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(requisicao):
-	return HttpResponse('Olá Mundo!')
+	return render(requisicao, 'base/index.html')
+
+def end(requisicao):
+	return render(requisicao, 'base/end.html')
+
+def game(requisicao, indice):
+	contexto = {'indice_da_questao': indice}
+	return render(requisicao, 'base/game.html', context = contexto)
+	
